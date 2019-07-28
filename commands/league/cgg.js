9514a60f0e59d73698448c0ff5d53e44146479b0
robertomarnegron <robert.negron@upr.edu>
2019-07-28 03:39:29 -0400

    Organized functions and added some comments

---
+++ commands/league/cgg.js
@@ -0,0 +1,26 @@
+const commando = require('discord.js-commando');
+
+class championgg extends commando.Command {
+
+    constructor(client){
+        super(client, {
+            name: 'cgg',
+            group: 'league',
+            memberName: 'cgg',
+            description: 'Generates hyperlink for champion given as input.' 
+        });
+    }
+    async run(message, args){
+        if(message.author.bot) return;
+        if(message.channel.type === "dm") return;
+
+        let messageArray = message.content.split(" ");
+        let command = messageArray[0];
+        let champion = messageArray[1];
+        let args1 = messageArray.slice(1);
+        message.channel.send("https://champion.gg/champion/" + champion + "/Middle?league=");
+        return;
+    }
+}
+
+module.exports = championgg;
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+
+//Import libraries and key
 const botSettings = require("./botsettings.json");
-const Discord = require('discord.js');
-const commando = require('discord.js-commando');
-const bot1 = new commando.Client();
+const Discord = require('discord.js-commando');
 
-bot1.registry.registerGroup('random', 'Random');
-bot1.registry.registerDefaults();
-bot1.registry.registerCommandsIn(__dirname + "/commands");
-
+//Starts bot's instance
 const bot = new Discord.Client({disableEveryone: true});
 
+//creates groups
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerGroup('league', 'League')
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+//let's you know bot is running and generates a link you can use to add bot to your server
 bot.on("ready", async () => {
     console.log(`Bot is ready ! ${bot.user.username}`);
     try{
@@ -19,29 +23,7 @@ bot.on("ready", async () => {
     }
 });
 
-bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let champion = messageArray[1];
-    let args = messageArray.slice(1);
-
-    if(!command.startsWith(botSettings.prefix)) return;
-
-    if(command === `${botSettings.prefix}cgg`){
-    message.channel.send("https://champion.gg/champion/" + champion + "/Middle?league=");
-       return;
-    }
-    if(command === `${botSettings.prefix}ph`){
-        message.channel.send("https://www.pornhub.com/categories/" + champion);
-           return;
-        }
-});
-
-
-
+//bot log in
 bot.login(botSettings.token);
 
 
